fb165bcacc48f90f102ebeceeb2ff1fadbddcfba
KhoaPC <luongvankhoapc@gmail.com>
2022-11-16 08:14:57 +0700

    22Nov16 Commit#1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Covid from "./views/Covid";
+import Blog from "./views/Blog";
+import DetailBlog from "./views/DetailBlog";
+import Todo from "./views/Todo";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleEventClick = (event) => {
+    if (!value) {
+      return;
+    }
+
+    let newTodo = {
+      id: Math.floor(Math.random() * 100000 + 1),
+      title: value,
+    };
+    setTodos([...todos, newTodo]);
+    setValue("");
+  };
+
+  const handleOnchangeInput = (event) => {
+    setValue(event.target.value);
+  };
+
+  const deleteDataTodo = (id) => {
+    let currentTodos = todos;
+    currentTodos = currentTodos.filter((item) => item.id !== id);
+    setTodos(currentTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Routes>
+          <Route path="/" exact element={<Covid></Covid>}></Route>
+          
+          <Route path="/blog"  element={<Blog></Blog>}></Route>
+          {/* <Route path="/blog/:id"  element={<DetailBlog></DetailBlog>}></Route> */}
+         
+          <Route
+            path="/todo"
+            element={
+              <>
+                {" "}
+                <Todo
+                  todos={todos}
+                  title={"All todos"}
+                  deleteDataTodo={deleteDataTodo}
+                />
+                <input
+                  type="text"
+                  value={value}
+                  onChange={(event) => handleOnchangeInput(event)}
+                />
+                <button
+                  type="button"
+                  onClick={(event) => handleEventClick(event)}
+                >
+                  Click me
+                </button>
+              </>
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <>
+      <div className="topnav">
+        <a href="/">Home</a>
+        <a href="/todo">Todo Apps</a>
+        <a href="/blog">Blog</a>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
