b85601d8e7ce9bb812306560da7ce2e75fbf2b0f
KhoaPC <luongvankhoapc@gmail.com>
2022-11-18 16:53:57 +0700

    22Nov UPDATE component `Todo`

--- src/App.js
+++ src/App.js
@@ -17,32 +17,6 @@ import Nav from "./components/Nav/Nav";
 import YoutubeSearch from "./views/YoutubeSearch";
 
 function App() {
-  const [value, setValue] = useState("");
-  const [todos, setTodos] = useState([]);
-
-  const handleEventClick = (event) => {
-    if (!value) {
-      return;
-    }
-
-    let newTodo = {
-      id: Math.floor(Math.random() * 100000 + 1),
-      title: value,
-    };
-    setTodos([...todos, newTodo]);
-    setValue("");
-  };
-
-  const handleOnchangeInput = (event) => {
-    setValue(event.target.value);
-  };
-
-  const deleteDataTodo = (id) => {
-    let currentTodos = todos;
-    currentTodos = currentTodos.filter((item) => item.id !== id);
-    setTodos(currentTodos);
-  };
-
   return (
     <Router>
       <Nav></Nav>
@@ -59,29 +33,7 @@ function App() {
             element={<YoutubeSearch></YoutubeSearch>}
           ></Route>
 
-          <Route
-            path="/todo"
-            element={
-              <>
-                <Todo
-                  todos={todos}
-                  title={"All todos"}
-                  deleteDataTodo={deleteDataTodo}
-                />
-                <input
-                  type="text"
-                  value={value}
-                  onChange={(event) => handleOnchangeInput(event)}
-                />
-                <button
-                  type="button"
-                  onClick={(event) => handleEventClick(event)}
-                >
-                  Click me
-                </button>
-              </>
-            }
-          ></Route>
+          <Route path="/todo" element={<Todo />}></Route>
         </Routes>
       </div>
     </Router>
--- src/views/Todo.js
+++ src/views/Todo.js
@@ -1,31 +1,62 @@
-const Todo = (props) => {
-    //properties 
-    // parent => child, top => bottom
-    const { todos, title, deleteDataTodo } = props;
+import { useState } from "react";
 
-    const handleDelete = (id) => {
-        deleteDataTodo(id)
+const Todo = () => {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleEventClick = (event) => {
+    if (!value) {
+      return;
     }
-    return (
-        <div className="todos-container">
-            <div className="title">
-                {title}
+
+    let newTodo = {
+      id: Math.floor(Math.random() * 100000 + 1),
+      title: value,
+    };
+    setTodos([...todos, newTodo]);
+    setValue("");
+  };
+
+  const handleOnchangeInput = (event) => {
+    setValue(event.target.value);
+  };
+
+  const deleteDataTodo = (id) => {
+    let currentTodos = todos;
+    currentTodos = currentTodos.filter((item) => item.id !== id);
+    setTodos(currentTodos);
+  };
+
+  const handleDelete = (id) => {
+    deleteDataTodo(id);
+  };
+  return (
+    <>
+      <div className="todos-container">
+        <div className="title">All Todo</div>
+        {todos.map((todo) => {
+          return (
+            <div key={todo.id}>
+              <li className="todo-child">
+                {todo.title}
+                <span onClick={() => handleDelete(todo.id)}>   x</span>
+              </li>
             </div>
-            {todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <li className="todo-child"> {todo.title}
-                            &nbsp;  &nbsp; <span onClick={() => handleDelete(todo.id)}>x</span>
-                        </li>
+          );
+        })}
 
-                    </div>
-                )
-            })}
-
-            <hr />
-
-        </div>
-    )
-}
+        <hr />
+      </div>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => handleOnchangeInput(event)}
+      />
+      <button type="button" onClick={(event) => handleEventClick(event)}>
+        Click me
+      </button>
+    </>
+  );
+};
 
 export default Todo;
