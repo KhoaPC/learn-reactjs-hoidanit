2d6f2d953b88820e828ed521a00947b5275eaeec
KhoaPC <luongvankhoapc@gmail.com>
2022-11-21 07:43:39 +0700

    22Nov21 DOCS add component `DetailBlog`

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,34 +1,34 @@
-import { useParams, useLocation } from 'react-router-dom';
-import useFetch from '../utils/fetch';
-import './Blog.css';
-import { Link} from 'react-router-dom';
+import { useParams, useLocation } from "react-router-dom";
+import useFetch from "../utils/fetch";
+import "./Blog.css";
+import { Link } from "react-router-dom";
 const DetailBlog = () => {
-    let { id } = useParams();
+  let { id } = useParams();
 
-    const { data: dataBlogDetail, isLoading, isError }
-        = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false)
+  const {
+    data: dataBlogDetail,
+    isLoading,
+    isError,
+  } = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false);
 
-    return (
-        <>
-            <div> <span > </span>
-            <Link to={'/blog'}>-- Back</Link>
+  return (
+    <>
+      <div>
+        <Link to={"/blog"}>-- Back</Link>
+      </div>
+      <div className="blog-detail">
+        {dataBlogDetail && (
+          <>
+            <div className="title">
+              Blog ID: {id} ---{" "}
+              {isLoading === true ? "Loading data ..." : dataBlogDetail.title}
             </div>
-            <div className="blog-detail">
-                {dataBlogDetail &&
-                    <>
-                        <div className="title">
-                            Blog ID: {id} ---  {isLoading === true ? 'Loading data ...' : dataBlogDetail.title}
-                        </div>
-                        <div className="content">
-                            {dataBlogDetail.body}
-                        </div>
-
-                    </>
-                }
-
-            </div>
-        </>
-    )
-}
+            <div className="content">{dataBlogDetail.body}</div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default DetailBlog;
