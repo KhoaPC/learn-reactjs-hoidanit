d890f415c0618fee1cc0666ec57dcb061ff3c681
KhoaPC <luongvankhoapc@gmail.com>
2022-11-21 07:20:48 +0700

    22Nov21 DOCS update page `detailBlog`

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,40 +1,34 @@
-// import { useParams, useHistory } from 'react-router-dom';
-// import useFetch from '../customize/fetch';
-// import './Blog.css';
+import { useParams, useLocation } from 'react-router-dom';
+import useFetch from '../customize/fetch';
+import './Blog.css';
+import { Link} from 'react-router-dom';
+const DetailBlog = () => {
+    let { id } = useParams();
 
-// const DetailBlog = () => {
-//     let { id } = useParams();
-//     let history = useHistory();
+    const { data: dataBlogDetail, isLoading, isError }
+        = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false)
 
+    return (
+        <>
+            <div> <span > </span>
+            <Link to={'/blog'}>-- Back</Link>
+            </div>
+            <div className="blog-detail">
+                {dataBlogDetail &&
+                    <>
+                        <div className="title">
+                            Blog ID: {id} ---  {isLoading === true ? 'Loading data ...' : dataBlogDetail.title}
+                        </div>
+                        <div className="content">
+                            {dataBlogDetail.body}
+                        </div>
 
+                    </>
+                }
 
-//     const { data: dataBlogDetail, isLoading, isError }
-//         = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false)
+            </div>
+        </>
+    )
+}
 
-//     const handleBackData = () => {
-//         history.push("/blog");
-//     }
-
-
-//     return (
-//         <>
-//             <div> <span onClick={handleBackData}>&lt;-- Back </span></div>
-//             <div className="blog-detail">
-//                 {dataBlogDetail &&
-//                     <>
-//                         <div className="title">
-//                             Blog ID: {id} ---  {isLoading === true ? 'Loading data ...' : dataBlogDetail.title}
-//                         </div>
-//                         <div className="content">
-//                             {dataBlogDetail.body}
-//                         </div>
-
-//                     </>
-//                 }
-
-//             </div>
-//         </>
-//     )
-// }
-
-// export default DetailBlog;
+export default DetailBlog;
